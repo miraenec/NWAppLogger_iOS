05111aa47faef775e7e5c863e6c73ae76e8fd832
Minjun <>
2021-11-22 09:17:34 +0900

    Fix : shared Objective-C 사용 가능 하도록

--- NWAppLogger/Classes/NWAppLogger.swift
+++ NWAppLogger/Classes/NWAppLogger.swift
@@ -6,6 +6,7 @@ public class NWAppLogger : NSObject {
     fileprivate var xtUid:String = ""
     fileprivate var ssId:String = ""
     
+    @objc
     public static let shared = NWAppLogger()
     
     @objc
